06a0a8e53870e75e32f93277b72fea11e41e697f
enriquechaacb <105406250+enriquechaacb@users.noreply.github.com>
2025-01-20 17:10:20 -0600

    User view

--- app/src/main/java/com/app/telomobileapp/ui/userprofile/Userprofile.kt
+++ app/src/main/java/com/app/telomobileapp/ui/userprofile/Userprofile.kt
@@ -1,21 +1,54 @@
 package com.app.telomobileapp.ui.userprofile
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.app.telomobileapp.R
+import com.app.telomobileapp.data.session.SessionManager
+import com.app.telomobileapp.databinding.ActivityAccountstateBinding
+import com.app.telomobileapp.databinding.ActivityUserprofileBinding
+import com.app.telomobileapp.ui.base.BaseActivity
 
-class Userprofile : AppCompatActivity() {
+class Userprofile : BaseActivity() {
+    private lateinit var binding: ActivityUserprofileBinding
+    private lateinit var sessionManager: SessionManager
+    private var licencia: String = ""
+
+    override fun getLayoutResourceId(): Int = R.layout.activity_userprofile
+    override fun getActivityTitle(): String = "Mi Usuario"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_userprofile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityUserprofileBinding.bind(findViewById(R.id.user_profile_container))
+        sessionManager = SessionManager(this)
+        licencia = sessionManager.getLicencia().toString()
+        //loadAccountGrid()
+//        val spinner = findViewById<Spinner>(R.id.spinnerFiltro)
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.filtro_estadocuenta,
+//            android.R.layout.simple_spinner_item
+//        ).also { adapter ->
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            spinner.adapter = adapter
+//        }
+//        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+//                when (pos) {
+//                    0 -> loadAccountList(1)
+//                    1 -> loadAccountList(2)
+//                    2 -> loadAccountList(3)
+//                }
+//            }
+//
+//            override fun onNothingSelected(parent: AdapterView<*>) {
+//                // Opcional: Manejar cuando no hay selección
+//            }
+//        }
     }
 }
